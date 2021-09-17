346174b345d44aa3d8e98eee88b42260ee4da676
Alivia <withalivia@gmail.com>
2021-09-16 20:53:59 -0700

    Checking initial changes

--- ClassicTableView/ViewController.swift
+++ ClassicTableView/ViewController.swift
@@ -7,12 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tblView: UITableView!
+    
+    let arr = ["Alivia", "Mark", "Hal", "maya"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tblView.delegate = self
+        tblView.dataSource = self
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tblView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = arr[indexPath.row]
+        return cell
+    }
+    
+    
+    
+   
 
 
 }
